c12922ae0f93e2e3823ee4633848b4cf69dac0b0
Rajeev Patil <rajeevvpatil899@gmail.com>
2022-09-22 18:00:19 -0700

    contact module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ArtistsComponent } from './artists/artists.component';
 import { AboutComponent } from './about/about.component';
 import { SpotlightComponent } from './gallery/spotlight/spotlight.component';
 import { StoreModule } from '@ngrx/store';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { StoreModule } from '@ngrx/store';
     GalleryComponent,
     ArtistsComponent,
     AboutComponent,
-    SpotlightComponent
+    SpotlightComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
