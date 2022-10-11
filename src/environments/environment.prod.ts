86cdcce55d62657d8076f278cd537f2fa76eb0ef
Rajeev Patil <rajeevvpatil899@gmail.com>
2022-10-10 17:30:47 -0700

    env file update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,18 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyCzmJKUt454FlX-bNrkhphgJ-lAWXhfMKA",
-    authDomain: "taylor-ink.firebaseapp.com",
-    projectId: "taylor-ink",
-    storageBucket: "taylor-ink.appspot.com",
-    messagingSenderId: "615541487828",
-    appId: "1:615541487828:web:a19e61bac432e239f42341",
-    measurementId: "G-6DF1YLGP1S"
-  }
+  firebaseConfig: {
+    apiKey: 'AIzaSyCzmJKUt454FlX-bNrkhphgJ-lAWXhfMKA',
+
+    authDomain: 'taylor-ink.firebaseapp.com',
+
+    projectId: 'taylor-ink',
+
+    storageBucket: 'taylor-ink.appspot.com',
+
+    messagingSenderId: '615541487828',
+
+    appId: '1:615541487828:web:a19e61bac432e239f42341',
+
+    measurementId: 'G-6DF1YLGP1S',
+  },
 };
